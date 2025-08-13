7490d7842f1b90bc5eb95f39b4e38ec2e22f6185
Sergey Aksenov <sergeax@gmail.com>
2025-08-13 21:33:00 +0200

    ci: deploy to Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://zero-call.org',
+  // No base path needed since this will be the root domain
+  output: 'static',
+  build: {
+    assets: 'assets'
+  }
+});
